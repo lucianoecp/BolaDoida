c846836aa5292d6c71b2c3cd8097ec095132907f
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-11-19 11:20:45 -0300

    Update

---
+++ Bola.java
@@ -0,0 +1,6 @@
+public class Bola extends FiguraGeometrica{
+    public Bola(int altura,int largura){
+        super(altura,largura);
+    }
+   
+}
--- Main.java
+++ Main.java
@@ -1,18 +1,26 @@
-public class Main {
-    public static void main(String[] args) throws InterruptedException
-    {
-        Cenario cenario = new Cenario("Bola Doida");
-        App app = new App();
-        
-        cenario.add(app);
-        cenario.setSize(640, 480);
-        cenario.setVisible(true);
-        cenario.setDefaultCloseOperation();
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
+public class Main {
+   
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args) throws InterruptedException
+    {   
+        JFrame area = new JFrame("Bola Doida");
+        Bola bola = new Bola(640,480);
+        Movimento app = new Movimento(area);
+       
+        area.add(app);
+        area.setSize(bola.getLargura(), bola.getAltura());
+        area.setVisible(true);
+        area.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         while(true)
         {
             app.moveBola(area);
             app.repaint();
-            Thread.sleep(vel);
+            Thread.sleep(app.getVel());
         }
+}
 }
