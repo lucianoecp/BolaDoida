00e2acd409656431b43795248719ced7818cd24c
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-11-21 08:11:06 -0300

    Update Main.java

--- BolaDoida/src/Main.java
+++ BolaDoida/src/Main.java
@@ -1,2 +1,21 @@
 package poo.anarquia.parkour;
 
+public class Main
+{
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        Game app = new Game(5);
+        Relatorio rel = new Relatorio();
+        Cenario area = new Cenario(app, rel);
+
+        while(true)
+        {
+            app.movimento();
+            app.repaint();
+            rel.setTxtNumColisoes(app.getNumColisoes());
+            rel.setTxtCordenadas(app.getPosX(), app.getPosY());
+            Thread.sleep(app.getVelocidade());
+        }
+    }
+}
